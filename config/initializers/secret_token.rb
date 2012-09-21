6e931554ef0dbb2bf997770b0b14dfb14d3a8272
yorthehunter <boru.design@gmail.com>
2012-09-21 10:27:18 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BorudesignRedux::Application.config.secret_token = 'bcacf1ae27cbd3133d36329c72682d10fbffd090fd0247b07efa2a1a5748ab1150c7b049a2065f3d8f973b5341998d5c121441f17eb9ecc77f33967c4abc1cca'
